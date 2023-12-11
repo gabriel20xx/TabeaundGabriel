c4914df66d1f9e220be9b1eb926e8fe01828ef9e
Gabriel <gabrielfrnz31@gmail.com>
2023-12-11 13:39:41 +0100

    fix

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php
+d<?php
 
 $imageFolder = './img';
 
@@ -128,6 +128,7 @@ if ($unlocked) {
     echo '<div class="container d-flex flex-column align-items-center justify-content-center text-center vh-100">';
     echo '<h1>Please enter pin code</h1>';
     echo '<form id="pin_input" action="/index.php" method="post">';
+    echo '<div class="container d-flex flex-column>';
     echo '<div>';
     echo '<input id="pin1" name="pin1" type="number" step="1" min="0" max="9" autocomplete="no" maxlength="1" class="pin-input" />';
     echo '<input id="pin2" name="pin2" type="number" step="1" min="0" max="9" autocomplete="no" maxlength="1" class="pin-input" />';
@@ -135,6 +136,7 @@ if ($unlocked) {
     echo '<input id="pin4" name="pin4" type="number" step="1" min="0" max="9" autocomplete="no" maxlength="1" class="pin-input" />';
     echo '</div>';
     echo '<button type="submit" name="submit" class="btn btn-primary">Submit</button>';
+    echo '</div>';
     echo '</form>';
     echo '</div>';
 
