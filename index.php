79120e569601b40dc1d760c61777a272dff8df7e
Gabriel <gabrielfrnz31@gmail.com>
2023-12-11 13:44:38 +0100

    fix

--- index.php
+++ index.php
@@ -130,10 +130,10 @@ if ($unlocked) {
     echo '<div class="container d-flex flex-column">';
     echo '<h1>Please enter pin code</h1>';
     echo '<div>';
-    echo '<input id="pin1" name="pin1" type="number" step="1" min="0" max="9" autocomplete="no" maxlength="1" class="pin-input" />';
-    echo '<input id="pin2" name="pin2" type="number" step="1" min="0" max="9" autocomplete="no" maxlength="1" class="pin-input" />';
-    echo '<input id="pin3" name="pin3" type="number" step="1" min="0" max="9" autocomplete="no" maxlength="1" class="pin-input" />';
-    echo '<input id="pin4" name="pin4" type="number" step="1" min="0" max="9" autocomplete="no" maxlength="1" class="pin-input" />';
+    echo '<input id="pin1" name="pin1" type="number" step="1" min="0" max="9" autocomplete="no" maxlength="1" class="pin-input m-2" />';
+    echo '<input id="pin2" name="pin2" type="number" step="1" min="0" max="9" autocomplete="no" maxlength="1" class="pin-input m-2" />';
+    echo '<input id="pin3" name="pin3" type="number" step="1" min="0" max="9" autocomplete="no" maxlength="1" class="pin-input m-2" />';
+    echo '<input id="pin4" name="pin4" type="number" step="1" min="0" max="9" autocomplete="no" maxlength="1" class="pin-input m-2" />';
     echo '</div>';
     echo '<button type="submit" name="submit" class="btn btn-primary">Submit</button>';
     echo '</div>';
