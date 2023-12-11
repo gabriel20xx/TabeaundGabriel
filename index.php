bcf432c8b2285e5780c43b1d8c0052123e17f55e
Gabriel <gabrielfrnz31@gmail.com>
2023-12-11 13:32:35 +0100

    fix

--- index.php
+++ index.php
@@ -130,8 +130,8 @@ if ($unlocked) {
     echo '<div class="row">';
     echo '<h1>Please enter pin code</h1>';
     echo '</div>';
-    echo '<div class="row align-items-center justify-content-center">';
     echo '<form id="pin_input" action="/index.php" method="post">';
+    echo '<div class="row d-flex align-items-center justify-content-center mb-2">';
     echo '<input id="pin1" name="pin1" type="number" step="1" min="0" max="9" autocomplete="no" maxlength="1" class="pin-input" />';
     echo '<input id="pin2" name="pin2" type="number" step="1" min="0" max="9" autocomplete="no" maxlength="1" class="pin-input" />';
     echo '<input id="pin3" name="pin3" type="number" step="1" min="0" max="9" autocomplete="no" maxlength="1" class="pin-input" />';
