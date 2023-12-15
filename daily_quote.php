3b8581e16e850e3d0643a2655787a39cdeb5da9b
Gabriel <gabrielfrnz31@gmail.com>
2023-12-15 09:01:17 +0100

    fix

--- daily_quote.php
+++ daily_quote.php
@@ -1,26 +1,62 @@
 <?php
+function call_api($method, $url, $data = false, $api_key = null)
+{
+    $curl = curl_init();
 
-$apiKey = 'WfvXGr1LNov5Rgj19vzppLhAAhBTwlx43v1x0L9a';
-$quoteFilename = 'daily_quote.txt';
-$category = 'inspire';
+    switch ($method) {
+        case "POST":
+            curl_setopt($curl, CURLOPT_POST, 1);
 
-if (file_exists($quoteFilename) && date('Y-m-d', filemtime($quoteFilename)) == date('Y-m-d')) {
-    $quote = file_get_contents($quoteFilename);
-} else {
-    $apiUrl = 'http://quotes.rest/qod.json?category=' . $category;
-    $response = file_get_contents($apiUrl);
-    $data = json_decode($response, true);
-
-    if ($data && isset($data['contents']['quotes'][0]['quote'])) {
-        $quote = $data['contents']['quotes'][0]['quote'];
-
-        file_put_contents($quoteFilename, $quote);
-    } else {
-        $quote = 'Failed to fetch the daily quote.';
+            if ($data) {
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+            }
+            break;
+        case "PUT":
+            curl_setopt($curl, CURLOPT_PUT, 1);
+            break;
+        default:
+        if ($data) {
+            if (!is_array($data)) {
+                die('Error: $data should be an array.');
+            }
+        
+            $url .= '?' . http_build_query($data);
+        }
     }
+
+    $headers = ['Content-Type: application/json'];
+    if (!empty($api_key)) {
+        $headers[] = 'X-Theysaidso-Api-Secret: ' . $api_key;
+    }
+
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+    $result = curl_exec($curl);
+
+    curl_close($curl);
+
+    return $result;
 }
 
-echo '<blockquote>';
-echo '<p>' . $quote . '</p>';
-echo '</blockquote>';
+// Replace 'YOUR_API_KEY' with your actual API key
+$api_key = 'YOUR_API_KEY';
+
+// They Said So API endpoint for a random quote with a specific category
+$qod_result = call_api("GET", "https://quotes.rest/qod?category=inspire", false, $api_key);
+
+// Decode the JSON response
+$qod_data = json_decode($qod_result, true);
+
+// Check if the request was successful
+if ($qod_data && isset($qod_data['contents']['quotes'][0]['quote'])) {
+    $quote = $qod_data['contents']['quotes'][0]['quote'];
+    echo '<blockquote>';
+    echo '<p>' . $quote . '</p>';
+    echo '</blockquote>';
+} else {
+    // Display an error message if the request fails
+    echo 'Failed to fetch the daily quote.';
+}
 ?>
