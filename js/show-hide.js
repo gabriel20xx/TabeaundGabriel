9c9f9058038118a023ab2dd4d35f8364b8ef688d
Gabriel <gabrielfrnz31@gmail.com>
2023-12-15 11:16:56 +0100

    fix

--- js/show-hide.js
+++ js/show-hide.js
@@ -1,22 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
     var starButton = document.getElementById("starButton");
     var quoteContainer = document.getElementById("quoteContainer");
+    var lockButton = document.getElementById("lockButton");
+    var lockContainer = document.getElementById("lockContainer");
+    var envelopeButton = document.getElementById("envelopeButton");
+    var envelopeContainer = document.getElementById("envelopeContainer");
+
     starButton.addEventListener("click", function () {
         toggleContainer(quoteContainer);
 
         hideOtherContainers([envelopeContainer, lockContainer]);
     });
 
-    var lockButton = document.getElementById("lockButton");
-    var lockContainer = document.getElementById("lockContainer");
     lockButton.addEventListener("click", function () {
         toggleContainer(lockContainer);
 
         hideOtherContainers([quoteContainer, envelopeContainer]);
     });
 
-    var envelopeButton = document.getElementById("envelopeButton");
-    var envelopeContainer = document.getElementById("envelopeContainer");
     envelopeButton.addEventListener("click", function () {
         toggleContainer(envelopeContainer);
         
