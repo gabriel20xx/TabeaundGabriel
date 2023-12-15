6b7e45d45d14a3695025f2bd390357c2857b8b05
Gabriel <gabrielfrnz31@gmail.com>
2023-12-15 11:10:00 +0100

    test

--- js/pin.js
+++ js/pin.js
@@ -2,15 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
     var pinInputs = document.querySelectorAll('.pin-input');
 
     pinInputs.forEach(function(input, index, array) {
-        input.addEventListener('input', function() {
-            var nextInput = array[index + 1];
-            if (nextInput) {
-                nextInput.focus();
-            } else {
-                input.blur();
-            }
-        });
-
         input.addEventListener('keydown', function(event) {
             if (event.key === 'Return' || event.key === 'Enter') {
                 var inputValue = input.value.trim();
@@ -20,6 +11,15 @@ document.addEventListener('DOMContentLoaded', function() {
                         prevInput.focus();
                     }
                 }
+            }
+        });
+
+        input.addEventListener('input', function() {
+            var nextInput = array[index + 1];
+            if (nextInput) {
+                nextInput.focus();
+            } else {
+                input.blur();
             }
         });
     });
