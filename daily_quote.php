cb557fae23a8fd8ea3084f1b1f771a38bf4c60b0
Gabriel <gabrielfrnz31@gmail.com>
2023-12-15 09:05:11 +0100

    add local copy

--- daily_quote.php
+++ daily_quote.php
@@ -40,23 +40,22 @@ function call_api($method, $url, $data = false, $api_key = null)
     return $result;
 }
 
-// Replace 'YOUR_API_KEY' with your actual API key
 $api_key = 'WfvXGr1LNov5Rgj19vzppLhAAhBTwlx43v1x0L9a';
+$category = 'inspire';
 
-// They Said So API endpoint for a random quote with a specific category
-$qod_result = call_api("GET", "https://quotes.rest/qod?category=inspire", false, $api_key);
-
-// Decode the JSON response
-$qod_data = json_decode($qod_result, true);
-
-// Check if the request was successful
-if ($qod_data && isset($qod_data['contents']['quotes'][0]['quote'])) {
-    $quote = $qod_data['contents']['quotes'][0]['quote'];
-    echo '<blockquote>';
-    echo '<p>' . $quote . '</p>';
-    echo '</blockquote>';
+if (file_exists($quoteFilename) && date('Y-m-d', filemtime($quoteFilename)) == date('Y-m-d')) {
+    $quote = file_get_contents($quoteFilename);
 } else {
-    // Display an error message if the request fails
-    echo 'Failed to fetch the daily quote.';
+    $qod_result = call_api("GET", "https://quotes.rest/qod?category=" . $category, false, $api_key);
+    $qod_data = json_decode($qod_result, true);
+    if ($qod_data && isset($qod_data['contents']['quotes'][0]['quote'])) {
+        $quote = $qod_data['contents']['quotes'][0]['quote'];
+        echo '<blockquote>';
+        echo '<p>' . $quote . '</p>';
+        echo '</blockquote>';
+    } else {
+        echo 'Failed to fetch the daily quote.';
+    }
 }
+
 ?>
