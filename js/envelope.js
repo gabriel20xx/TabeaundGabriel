d11879afa1390e42232768242e7c7b1e3cc5d32c
Gabriel <gabrielfrnz31@gmail.com>
2023-12-15 11:23:08 +0100

    fix

--- js/envelope.js
+++ js/envelope.js
@@ -2,7 +2,6 @@ current = 1;
 function toggleEnvelope(event) {
     const wrapper = event.currentTarget;
     const frames = document.querySelectorAll('.frame');
-    var frameContainer = document.getElementsByClassName("wrapper");
 
     if (wrapper.classList.contains('active')) {
         wrapper.classList.remove('active');
@@ -14,13 +13,9 @@ function toggleEnvelope(event) {
                 current = 0;
             }
 
-            wrapper.classList.add('d-none');
             current++;
-            frameContainer[current].classList.add("appear");
-            frameContainer[current].classList.remove("d-none");
         }, 2500);
     } else {
         wrapper.classList.add('active');
-        wrapper.classList.remove('appear');
     }
 }
