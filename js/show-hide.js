601eb6b8c289d2e1cb1e43ae987bc46411ed106b
Gabriel <gabrielfrnz31@gmail.com>
2023-12-15 11:05:36 +0100

    fix

--- js/show-hide.js
+++ js/show-hide.js
@@ -2,21 +2,34 @@ document.addEventListener("DOMContentLoaded", function () {
     var starButton = document.getElementById("starButton");
     var quoteContainer = document.getElementById("quoteContainer");
     starButton.addEventListener("click", function () {
-        quoteContainer.classList.toggle("d-none");
+        toggleContainer(quoteContainer);
+
+        hideOtherContainers([envelopeContainer, lockContainer]);
     });
 
     var lockButton = document.getElementById("lockButton");
     var lockContainer = document.getElementById("lockContainer");
     lockButton.addEventListener("click", function () {
-        lockContainer.classList.toggle("d-none");
+        toggleContainer(lockContainer);
+
+        hideOtherContainers([quoteContainer, envelopeContainer]);
     });
 
     var envelopeButton = document.getElementById("envelopeButton");
     var envelopeContainer = document.getElementById("envelopeContainer");
-    var frameContainer = document.getElementsByClassName("wrapper");
     envelopeButton.addEventListener("click", function () {
-        envelopeContainer.classList.toggle("d-none");
-        frameContainer[current].classList.toggle("appear");
-        frameContainer[current].classList.toggle("d-none");
+        toggleContainer(envelopeContainer);
+        
+        hideOtherContainers([quoteContainer, lockContainer]);
     });
+
+    function toggleContainer(container) {
+        container.classList.toggle("d-none");
+    }
+
+    function hideOtherContainers(containers) {
+        containers.forEach(function (container) {
+            container.classList.add("d-none");
+        });
+    }
 });
