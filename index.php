ff6fa1c0dccf170f460e2f680383e3e6c69fcdb4
Gabriel <gabrielfrnz31@gmail.com>
2023-12-18 16:18:41 +0100

    add more names

--- index.php
+++ index.php
@@ -97,11 +97,11 @@ if ($unlocked) {
     // Envelope container
     $data = [
         ["name" => "Gabriel", "message" => "You are the best person on earth. I love you with every heartbeat. I wish you the best. Make your wishes come true.", "color1" => "#0000ff", "color2" => "#ff9500", "color3" => "#00ffff"],
-        // Mami
+        ["name" => "Mami", "message" => "", "color1" => "#00000", "color2" => "#00000", "color3" => "#00000"],
         ["name" => "Papi", "message" => "Liebe Tabea<br>Ich wünsche Dir von ganzem Herzen ❤️ wunderbare Weihnachten🎄🎄Merry Christmas!🫂", "color1" => "#fdc500", "color2" => "#ffd500", "color3" => "#ffee32"],
         ["name" => "Diego", "message" => "Hey Schwöschterherz<br>Ich wünsche dir ganz schöni Wiehnachte und han dich ganz fest gern!❤️ bin immer für dich da!😘", "color1" => "#00000", "color2" => "#00000", "color3" => "#00000"],
         ["name" => "Roxy", "message" => "Liebi Tabea, für mi bisch du über die jahr wie zure chline schwöster worde. Ig bi so stouz uf di wie du di am witer- entwickle bisch und bi immer da faus du mi bruchsch!❤️", "color1" => "#023e8a", "color2" => "#0077b6", "color3" => "#0096c7"],
-        // Hanna
+        ["name" => "Hanna", "message" => "", "color1" => "#00000", "color2" => "#00000", "color3" => "#00000"],
         ["name" => "Alina", "message" => "Du bisch mir as Herz gwachse, ich schätze eusi SFrünschaft und wünsche Dir viel liebi im neue Jahr❤️", "color1" => "#38419D", "color2" => "#3887BE", "color3" => "#52D3D8"],
         ["name" => "Jeremy", "message" => "Ich kenn dich jetzt au scho es zitli, bisch en sehr tolle Mensch, blieb so wied bisch 🙂", "color1" => "#236969", "color2" => "#43A680", "color3" => "#74F6A7"],
         ["name" => "Sophia", "message" => "Du bisch min Sunneschii, 20 Jahr han ich dich a minere Sitte kah und hoffe uf witeri 100 Jahr mit dir! I love You!", "color1" => "#ad2bcc", "color2" => "#ba55d3", "color3" => "#c9a0dc"],
