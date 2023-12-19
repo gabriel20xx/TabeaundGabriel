81b4c9a024ad388e7f2493c14d39703f45ffd88c
Gabriel <gabrielfrnz31@gmail.com>
2023-12-19 09:10:23 +0100

    fix

--- index.php
+++ index.php
@@ -118,7 +118,7 @@ if ($unlocked) {
         echo '<div class="lid two" style="border-top: 100px solid ' . $entry['color3'] . ';" ></div>';
         echo '<div class="envelope" style="border-right: 150px solid ' . $entry['color1'] . '; border-bottom: 100px solid' . $entry['color1'] . '; border-left: 150px solid' . $entry['color1'] . '; " ></div>';
         echo '<div class="letter d-flex flex-column justify-content-between p-1">';
-        echo '<p class="message">' . $entry['message'] . '</p>';
+        echo '<p class="message mb-0">' . $entry['message'] . '</p>';
         echo '<p class="fst-italic name">- ' . $entry['name'] . '</p>';
         echo '</div>';
         echo '</div>';
