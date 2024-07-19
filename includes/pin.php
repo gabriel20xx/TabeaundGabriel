892b064e536f3137347f4c6c85660a03016ec277
gabriel20xx <gabrielfranz31@gmail.com>
2024-07-19 21:38:26 +0200

    fix

--- includes/pin.php
+++
@@ -1,9 +0,0 @@
-<?php
-$startDate = new DateTime('2022-06-23');
-$today = new DateTime();
-
-$interval = date_diff($startDate, $today);
-$daysSince = $interval->format('%a');
-
-$correctPin = str_pad($daysSince, 4, '0', STR_PAD_LEFT);
-?>
--- index.php
+++ index.php
@@ -13,6 +13,14 @@ if (isset($_GET['page'])) {
     $page = 'main';
 }
 
+$startDate = new DateTime('2022-06-23');
+$today = new DateTime();
+
+$interval = date_diff($startDate, $today);
+$daysSince = $interval->format('%a');
+
+$correctPin = str_pad($daysSince, 4, '0', STR_PAD_LEFT);
+
 include "parts/head.php";
 echo '<body class="vh-100 container d-flex flex-column align-items-center justify-content-between text-center">';
 
