7419e9bb2a375d5f0fe2c538d11414a57e974ff5
gabriel20xx <gabrielfranz31@gmail.com>
2024-07-19 19:09:13 +0200

    test

--- includes/delete.php
+++ includes/delete.php
@@ -1,13 +1,8 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['image'])) {
+    $galleryDir = realpath('../gallery/');
+
     $file = $_POST['image'];
-
-    // Make sure the path is relative to the gallery directory
-    $file = realpath($file);
-
-    // Ensure the file is in the gallery directory
-    if (strpos($file, realpath('../gallery/')) === 0 && file_exists($file)) {
-        unlink($file);
-    }
+    unlink($file);
 }
 header('Location: ../index.php?page=gallery');
